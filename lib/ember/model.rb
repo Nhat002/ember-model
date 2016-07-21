e78b2d78d6937b16424a3909f87fce499c1debef
Nhat Tran <vuxuannhattran@viki.com>
2016-07-21 14:23:42 +0800

    first commit

---
+++ lib/ember/model.rb
@@ -0,0 +1,7 @@
+require "ember/model/version"
+
+module Ember
+  module Model
+    # Your code goes here...
+  end
+end
---
+++ lib/ember/model/version.rb
@@ -0,0 +1,5 @@
+module Ember
+  module Model
+    VERSION = "0.1.0"
+  end
+end
